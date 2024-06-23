e2c1de6b1ade8fa49853afceb6d6de022750f0c0
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-23 10:06:30 -0600

    chore: Update file naming convention and handle existing file in upload route

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 const router = express.Router();
 
@@ -28,10 +29,24 @@ router.post('/', upload.single('image'), (req, res) => {
   if (!req.body.nss) {
     return res.status(400).send({ message: 'Please provide an NSS' });
   }
-  const imageUrl = `http://${req.hostname}:3008/uploads/${req.file.filename}`;
+  
+  const nss = req.body.nss;
+  const extension = path.extname(req.file.originalname);
+  const filename = `${nss}${extension}`;
+  const filePath = path.join(__dirname, '../../uploads', filename);
+
+  // Verificar si el archivo ya existe y reemplazarlo si es necesario
+  if (fs.existsSync(filePath)) {
+    fs.unlinkSync(filePath);
+  }
+
+  // Mover el archivo subido al destino final
+  fs.renameSync(req.file.path, filePath);
+
+  const imageUrl = `http://${req.hostname}:3008/uploads/${filename}`;
   res.send({
     imageUrl: imageUrl,
-    filename: req.file.filename
+    filename: filename
   });
 });
 
